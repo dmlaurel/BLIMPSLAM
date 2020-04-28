9dcdedb5e3d403757c1930b57f4c83397500c9ae
shadowbreaker7 <chengfeng_xu_9610@126.com>
2020-04-28 01:45:51 -0400

    complementary files supporting videos

---
+++ video_slam_complementary/myvideo.cpp
@@ -0,0 +1,57 @@
+// Autohor gaoxiang12@github.com
+// Created by xiang on 11/29/17.
+//
+
+// open video file，pass images to ORB-SLAM2 to relocalize
+
+// include opencv
+#include <opencv2/opencv.hpp>
+
+// ORB-SLAM interface
+#include "System.h"
+
+#include <string>
+#include <chrono>   // for time stamp
+#include <iostream>
+
+using namespace std;
+
+// parameter and dictionary files
+// change to right path on your computer
+string parameterFile = "PATH TO /Drone_Cam.yaml";
+string vocFile = "/home/hostname/ORB_SLAM2/Vocabulary/ORBvoc.txt";
+
+// video file
+string videoFile = "PATH TO /flight_video_indoor.avi";
+
+int main(int argc, char **argv) {
+
+    // declare ORB-SLAM 
+    ORB_SLAM2::System SLAM(vocFile, parameterFile, ORB_SLAM2::System::MONOCULAR, true);
+
+    // images from video
+    cv::VideoCapture cap(videoFile);    // change to 1 if you want to use USB camera.
+
+    // system time
+    auto start = chrono::system_clock::now();
+
+    while (1) {
+        cv::Mat frame;
+        cap >> frame;   // camera data
+        if ( frame.data == nullptr )
+            break;
+
+        // rescale because image is too large
+        cv::Mat frame_resized;
+        cv::resize(frame, frame_resized, cv::Size(640,360));
+
+        auto now = chrono::system_clock::now();
+        auto timestamp = chrono::duration_cast<chrono::milliseconds>(now - start);
+        SLAM.TrackMonocular(frame_resized, double(timestamp.count())/1000.0);
+        cv::waitKey(30);
+    }
+
+    SLAM.Shutdown();
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+    return 0;
+}
